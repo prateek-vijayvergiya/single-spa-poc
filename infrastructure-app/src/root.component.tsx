c5342af1a7bc1d7e1178eadd28bf33e3cd57e5c4
prateek-vijayvergiya <prateek.vijayvergiya@dell.com>
2022-01-29 16:19:49 +0530

    Fixed some css

--- infrastructure-app/src/root.component.tsx
+++ infrastructure-app/src/root.component.tsx
@@ -4,11 +4,13 @@ import Network from "./pages/network";
 import Storage from "./pages/storage";
 
 export default function Root(props) {
-  return <BrowserRouter>
-  <Routes>
-    <Route path="/infra/clusters" element={<Clusters/>}/>
-    <Route path="/infra/storage" element={<Storage/>}/>
-    <Route path="/infra/network" element={<Network/>}/>
-  </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/infra/clusters" element={<Clusters />} />
+        <Route path="/infra/storage" element={<Storage />} />
+        <Route path="/infra/network" element={<Network />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
